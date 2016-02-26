5b597297dbf92a913326777f9105d283eb76ea29
Antonio <agarcia@ninjacodestudio.com>
2016-02-26 20:52:34 +0100

    UI Pack + Main Menu

---
+++ Assets/Scripts/GEAnimNormalizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GEAnimNormalizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        GEAnimSystem.Instance.m_GUISpeed = 1.0f;
+        GEAnimSystem.Instance.m_AutoAnimation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GEAnimStarter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GEAnimStarter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GEAnimSystem.Instance.MoveIn(this.transform, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
